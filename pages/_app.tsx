030afb1ebc8c62c90f693c8228559a9a56c6990f
Raf Ceuls <rceuls@gmail.com>
2020-08-07 20:46:50 +0200

    re-enabled login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,29 @@ import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import NavigationMenu from "../components/NavigationMenu";
 import { Container } from "semantic-ui-react";
-import { signin, useSession } from "next-auth/client";
+import { signIn, useSession } from "next-auth/client";
 import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   const [session, loading] = useSession();
 
   return (
-    <Provider session={pageProps.session}>
-      <NavigationMenu />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    </Provider>
+    <>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <Provider session={pageProps.session}>
+          <NavigationMenu />
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </Provider>
+      )}
+    </>
   );
 }
 
