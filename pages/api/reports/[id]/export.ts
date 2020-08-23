79542c836bbd205479c6938f8f876c6085899b53
Raf Ceuls <rceuls@gmail.com>
2020-08-23 10:37:21 +0200

    cleaned up dependencies

--- pages/api/reports/[id]/export.ts
+++ pages/api/reports/[id]/export.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import uploadS3File from "../../../../services/s3uploader";
 import { getSession } from "next-auth/client";
+import { getReport } from "../../../../services/database";
+import pdfkit from "pdfkit";
 
 export default async function exportToPdf(
   req: NextApiRequest,
@@ -14,14 +15,19 @@ export default async function exportToPdf(
   switch (req.method) {
     case "POST":
       const params = JSON.parse(req.body);
-      const [s3Url, filePath] = await uploadS3File(
-        params.fileName,
-        params.contentType
-      );
-      res.status(200).json({ uploadPath: s3Url, filePath });
+      const report = await getReport(params.id as string);
+      if (!report) {
+        res.status(404);
+      }
+      const reportPdf = await createPdf(report);
+      res.status(200).json({});
       break;
     default:
       res.setHeader("Allow", ["POST"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
+
+async function createPdf(report: Report) {
+  foreach();
 }
